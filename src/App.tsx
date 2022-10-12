6526626b5e57ce69b0f63baa49b88e6465dbb7ab
Robert <robellorin@gmail.com>
2022-10-12 21:45:43 +0500

    scroll fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import { ROUTES } from './routes/routes'
+import './App.css'
 
 function App() {
   return (
